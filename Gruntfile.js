8d73728e66b4681c347f70d3d64b1fbefa30f016
Tim Hartmann <tim.hartmann93@gmail.com>
2013-09-04 23:14:49 +0200

    refactor(*): added grunt and bower, renaming all
    -- added grunt with a Gruntfile.js
    -- configurate Gruntfile
    -- added bower for managing this package
    -- installed packages like jquery via bower
    -- removed mixins and use compass now
    -- use new convention of naming the modules, states and layouts
    -- new structure

---
+++ Gruntfile.js
@@ -0,0 +1,72 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      tasks: ['default']
+    },
+    sass: {
+      build: {
+        options: {
+          style: 'expanded',
+          compass: true
+        },
+        files: [
+          {
+            expanded: true,
+            cwd: 'assets',
+            src: ['assets/src/scss/application.scss'],
+            dest: 'assets/build/stylesheets/application.css',
+            filter: 'isFile'
+          }
+        ]
+      }
+    },
+    copy: {
+      main: {
+        expand: true,
+        src: ['assets/src/fonts/*'],
+        dest: 'assets/build/fonts',
+        filter: 'isFile',
+        flatten: true
+      }
+    },
+    cssmin: {
+      minify: {
+        expand: true,
+        cwd: 'assets/build/stylesheets/',
+        src: [
+          'application.css'
+        ],
+        dest: 'assets/build/stylesheets/',
+        ext: '.min.css'
+      }
+    },
+    concat: {
+      build: {
+        src: [
+          'assets/src/components/jquery/jquery.js',
+          'assets/src/js/app.js'
+        ],
+        dest: 'assets/build/javascript/application.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'assets/build/javascript/application.js',
+        dest: 'assets/build/javascript/application.min.js'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  // Default task(s).
+  grunt.registerTask('default', ['sass', 'cssmin', 'copy', 'concat', 'uglify']);
+
+};
--- site/templates/article.php
+++ site/templates/article.php
@@ -1,14 +1,14 @@
 <?php snippet('header') ?>
 <?php $date = strtotime($page->datetime()) ?>
 
-<section class="row" role="main">
-  <article>
-    <img src="<?php echo thumb($page->images()->find('thumb.jpg'), array('width' => 576, 'height' => 261, 'crop' => true), false) ?>" class="thumb">
-    <div class="matter">
+<main role="main">
+  <article class="m-post">
+    <img src="<?php echo thumb($page->images()->find('thumb.jpg'), array('width' => 576, 'height' => 261, 'crop' => true), false) ?>" class="m-post__thumb">
+    <div class="m-post__matter">
       <h2><?php echo $page->title() ?></h2>
       <time><?php echo date('d. M Y', $date) ?></time>
       <p><?php echo kirbytext($page->text()) ?></p>
     </div>
   </article>
-</section>
+</main>
 <?php snippet('footer')?>
